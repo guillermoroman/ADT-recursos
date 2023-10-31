19ff74cca032a93a220795c5ef21b77c3d21af36
guillermoroman <guillermoroman@outlook.com>
2023-10-31 19:26:18 +0100

    crear array dde Estudiantes

--- ADT_T2_recursos/adt_t2_recursos/src/Estudiante.java
+++ ADT_T2_recursos/adt_t2_recursos/src/Estudiante.java
@@ -1,8 +1,28 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 // Ejemplo de uso de RandomAccessFile como pequeña base de datos
 public class Estudiante {
     private int id;
     private String nombre;
     private double calificacion;
+
+    public static void main(String[] args) {
+        añadirEstudiantesDeEjemplo();
+    }
+
+    public static void añadirEstudiantesDeEjemplo() {
+
+        Estudiante[] estudiantes = {
+                new Estudiante(1, "Juan",  8.56),
+                new Estudiante(2, "María", 6.83),
+                new Estudiante(3, "Sara", 9.23),
+                new Estudiante(4, "Jacinto", 5.45),
+                new Estudiante(5, "Mario", 7.83)
+        };
+
+
+    }
 
     public Estudiante (int id, String nombre, double calificacion) {
         this.id = id;
